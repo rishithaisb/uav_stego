ca00207ca7672bf0632ef3057e6c27f97885a941
Rishitha Isbergaa <isbergaarishitha@gmail.com>
2025-03-09 10:01:38 +0530

    aes modis

--- algorithm/AES_encryption.py
+++ algorithm/AES_encryption.py
@@ -2,16 +2,19 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
+#Encrypt text using AES.
 def encrypt_aes(text, key):
-    """Encrypt text using AES."""
     cipher = AES.new(key, AES.MODE_CBC)
     ct_bytes = cipher.encrypt(pad(text.encode(), AES.block_size))
     return cipher.iv + ct_bytes  
 
+#Decrypt text using AES.
 def decrypt_aes(encrypted_data, key):
-    """Decrypt text using AES."""
     iv = encrypted_data[:AES.block_size] 
     ct = encrypted_data[AES.block_size:]  
     cipher = AES.new(key, AES.MODE_CBC, iv)
     pt = unpad(cipher.decrypt(ct), AES.block_size)
     return pt.decode()
+
+
+    
